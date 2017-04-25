6a3af65d5bc0fc4497ad64e4335d9a87880dbb80
RihardsPihards <Rihardsvv@gmail.com>
2017-04-25 15:42:43 +0300

    Add files via upload

---
+++ webSite/admin.php
@@ -0,0 +1,25 @@
+<?php
+include('classes/template.php');
+$template = new template();
+?>
+
+<head>
+    <?php template::getMeta(); ?>
+        <title><?php $template->showTitle(); ?></title>
+    <?php template::getLibs(); ?>
+</head>
+<body>
+    <?php template::getMenu(); ?>
+
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-3">
+                    <h1><font color="black" size="5px">Dzēst lietotāju:</font></h1>
+                        <input type="submit" class="button" value="Dzēst" name="deleteUser">
+                </div>
+            </div>
+        </div>
+
+    <?php template::getJsLibs(); ?>
+</body>
+</html>
