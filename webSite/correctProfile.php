78f2d44fa9c4f158898dc6e996ce386bd641a1c5
RihardsPihards <Rihardsvv@gmail.com>
2017-04-26 18:03:19 +0300

    Update correctProfile.php

--- webSite/correctProfile.php
+++ webSite/correctProfile.php
@@ -17,16 +17,16 @@ $template = new template();
                 <a href="" data-lightbox="gallery"><img src="" alt="profila bilde" /></a>
             </div> <br>
     <form action="correctProfile.php" method="post" enctype="multipart/form-data">
-            <label for="file">Izvēlieties bildi:</label>
+        <label for="file"><font color="#daa520">Izvēlieties bildi:</font></label>
             <input type="file" name="pic"/>
-            <input type="submit" class="button" value="Ielādēt!" />
+            <input type="submit" class="button" value="Ielādēt" />
     </form>
         </div>
         <br>
             <div class="row">
                 <div class="col-md-4 aboutMe">
                   <form>
-                      <h3><font color="black">Pastāsti par sevi:</font></h3>
+                      <h3><font color="#daa520">Pastāsti par sevi:</font></h3>
                         <input type="text"  name="userInfo"/>
                         <input type="submit" class="button" name="aboutMe" value="Saglabāt" />
                   </form>
